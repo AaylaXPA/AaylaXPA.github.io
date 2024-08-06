acd3873a6f867fe26cf61832f36ac18a88a8955f
ShadowXPA <51165769+ShadowXPA@users.noreply.github.com>
2024-08-06 11:45:46 -0400

    Change layout, add footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(() => {
     const selectNextBackground = () => {
-        const elements = $('.background')
+        const elements = $('.rain div')
         const count = elements.length
         const nextIndex = Math.floor(Math.random() * count)
         const nextElement = $(elements[nextIndex])
@@ -9,25 +9,24 @@ $(() => {
             return selectNextBackground()
         }
 
-        $('.background.active').removeClass('active')
+        $('.rain div.active').removeClass('active')
         nextElement.addClass('active')
         return nextIndex
     }
 
     const randomLightning = () => {
         setTimeout(() => {
-            $('.background').css('opacity', '.4')
+            $('.rain').addClass('lightning')
             setTimeout(() => {
-                $('.background').css('opacity', '.7')
+                $('.rain').removeClass('lightning')
                 randomLightning()
             }, 200)
         }, Math.floor(Math.random() * 10000) + 50)
     }
 
-    $('.background').each((index, element) => {
+    $('[data-bg]').each((index, element) => {
         const elem = $(element)
         const background = elem.attr('data-bg')
-        elem.css('opacity', '.7')
 
         elem.css('background-image', `url('img/${background}')`)
     })
